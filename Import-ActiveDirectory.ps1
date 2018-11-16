83a618fea46a7a11f68e5942feed2b88569cd7c6
Nikhil "SamratAshok" Mittal <nikhil.uitrgpv@gmail.com>
2018-11-16 19:41:06 +0530

    Update Import-ActiveDirectory.ps1

--- Import-ActiveDirectory.ps1
+++ Import-ActiveDirectory.ps1
@@ -2,19 +2,34 @@ function Import-ActiveDirectory
 {
 <#
 .SYNOPSIS
-ADModule Script which could import ActiveDirectory module
+ADModule Script which could import ActiveDirectory module without writing DLL to disk.
 
 .DESCRIPTION
-This script will import AD modules without writing files to disk.
+This script will import AD modules without writing DLL to disk. To use your own DLL byte array use the below commands:
+
+PS > [byte[]] $DLL = Get-Content -Encoding byte -path C:\ADModule\Microsoft.ActiveDirectory.Management.dll
+PS > [System.IO.File]::WriteAllLines(C:\ADModule\dll.txt, ([string]$DLL))
+
+It is always advised to load your own DLL ;)
 
 .PARAMETER ActiveDirectoryModule
-Path to the ActiveDirectoryModule dll.
+Path to the ActiveDirectoryModule DLL.
 
 .EXAMPLE
 PS > Import-ActiveDirectory
 
+Use the above command to load the DLL byte array already hard-coded in the script. 
+
 .EXAMPLE
-PS > Import-ActiveDirectory -ActiveDirectoryModule C:\test\Microsoft.ActiveDirectory.Management.dll
+PS > Import-ActiveDirectory -ActiveDirectoryModule C:\ADModule\Microsoft.ActiveDirectory.Management.dll
+
+Use the above path to load the Microsoft.ActiveDirectory.Management.dll from disk.
+
+.LINK
+https://github.com/samratashok/ADModule/pull/1
+https://github.com/samratashok/ADModule
+#>  
+
 #>
     [CmdletBinding()] Param(
         [Parameter(Position = 0, Mandatory = $False)]
